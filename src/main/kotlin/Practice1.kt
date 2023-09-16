01e0b4cb8e9d1753ce648c4f4772abe8bf76496d
semenyutenko <semenyutenko@gmail.com>
2023-09-17 00:56:35 +0300

    task_13

--- src/main/kotlin/Practice1.kt
+++ src/main/kotlin/Practice1.kt
@@ -3,3 +3,9 @@
 fun isLeapYear(year: Int): Boolean {
     return year % 400 == 0 || year % 4 == 0 && year % 100 != 0
 }
+
+// task_13
+
+fun countEmployees(vararg names: String): Int {
+    return names.size
+}
